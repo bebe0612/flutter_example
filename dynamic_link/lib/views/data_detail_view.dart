980d94fa5a2d50d91cd7c0b22ac0b7ac8c3de7ae
bebe <leebebe0612@gmail.com>
2021-04-01 10:06:53 +0900

    add preview

--- dynamic_link/lib/views/data_detail_view.dart
+++ dynamic_link/lib/views/data_detail_view.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class DataDetailView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Data detail"),
+        backgroundColor: Colors.red[300],
+      ),
+      body: Center(
+        child: Text("Data Received : "),
+      ),
+    );
   }
 }
--- dynamic_link/lib/views/home_view.dart
+++ dynamic_link/lib/views/home_view.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("Home")),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => null,
+      ),
+      body: Center(
+        child: Text("Home"),
+      ),
+    );
   }
 }

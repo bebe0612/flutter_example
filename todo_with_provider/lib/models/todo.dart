5d1a0a448bc7cceb2281cf97d50fb9ca3d2f9c08
bebe <leebebe0612@gmail.com>
2021-04-02 08:36:37 +0900

    add equatable package

--- todo_with_provider/lib/models/todo.dart
+++ todo_with_provider/lib/models/todo.dart
@@ -1,10 +1,14 @@
-class Todo {
+import 'package:equatable/equatable.dart';
+
+class Todo extends Equatable {
   final String id;
   final DateTime date;
   final String title;
   final String body;
   final bool isDone;
 
+  @override
+  List<Object> get props => [id];
   Todo({
     required this.id,
     required this.date,

6b58535718576dfd488e14b5efc6765f4372ec9c
bebe <leebebe0612@gmail.com>
2021-04-02 08:15:18 +0900

    initial commit

---
+++ todo_with_provider/lib/models/todo.dart
@@ -0,0 +1,15 @@
+class Todo {
+  final String id;
+  final DateTime date;
+  final String title;
+  final String body;
+  final bool isDone;
+
+  Todo({
+    required this.id,
+    required this.date,
+    required this.title,
+    required this.body,
+    required this.isDone,
+  });
+}

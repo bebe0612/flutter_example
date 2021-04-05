ca77dd6d4921778ab9575462f6b983e97ad18b45
bebe <leebebe0612@gmail.com>
2021-04-06 08:18:52 +0900

    add provider

--- todo_with_provider/lib/main.dart
+++ todo_with_provider/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_with_provider/providers/todo_provider.dart';
 import 'package:todo_with_provider/views/splash_view.dart';
 
 void main() => runApp(MyApp());
@@ -6,13 +8,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo With Provider',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (_) => TodoProvider(),
+      child: MaterialApp(
+        title: 'Todo With Provider',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SplashView(),
       ),
-      home: SplashView(),
     );
   }
 }
